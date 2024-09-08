ef8ec3805b5034e12ba183d4917fe11185dfbaba
elissandroa <elissandroa@hotmail.com>
2024-09-08 18:12:57 -0300

    Inserindo React-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { Home } from './components/pages/Home';
+import { Register } from './components/pages/Auth/Register';
+import { Login } from './components/pages/Auth/Login';
+
+
 
 function App() {
   return (
-    <div className="container">
-      <h1>Olá React</h1>
-    </div>
+    <Router>
+      <p><Link to={'/'}>Home</Link></p>
+      <p><Link to={'/login'}>Login</Link></p>
+      <p><Link to={'/register'}>Register</Link></p>
+      
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+
   );
 }
 
