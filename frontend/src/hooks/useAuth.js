f0f9fcf7fc5c05aa40d782665d1ce976a24dcd51
elissandroa <elissandroa@hotmail.com>
2024-09-09 07:49:48 -0300

    Criando hook de autenticação

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,21 @@
+import api from "../utils/api";
+import { useState, useEffect } from 'react'
+
+import React from 'react'
+
+import React from 'react'
+
+export default function useAuth() {
+    async function register(user) {
+        try {
+            const data = api.post('/users/register/', user).then((response) => {
+                return response.data;
+            })
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return { register }
+}
