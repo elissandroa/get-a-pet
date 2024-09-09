de47bc3891f36f2d9f62a59e4ee9ca33e2620812
elissandroa <elissandroa@hotmail.com>
2024-09-09 19:15:38 -0300

    Atualização do Usuário

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -3,13 +3,15 @@ import '../../form/Form.css'
 import { Input } from '../../form/Input'
 import './Profile.css'
 import React, { useEffect, useState } from 'react'
+import useFlashMessage from '../../../hooks/useFlashMessage'
 
 
 export const Profile = () => {
     const [user, setUser] = useState({});
     const [token] = useState(localStorage.getItem('token' || ""))
+    const { setFlashMessage } = useFlashMessage();
 
-    useEffect(()=>{
+    useEffect(() => {
         api.get('/users/checkuser', {
             headers: {
                 Authorization: `Bearer ${JSON.parse(token)}`
@@ -17,18 +19,38 @@ export const Profile = () => {
         }).then((response) => {
             setUser(response.data)
         })
-    },[token])
+    }, [token])
 
     const onFileChange = (e) => {
-
+        setUser({ ...user, [e.target.name]: e.target.files[0] })
     }
 
     const handleChange = (e) => {
-
+        setUser({ ...user, [e.target.name]: e.target.value })
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        let msgType = 'success';
+
+        const formData = new FormData();
+
+        await Object.keys(user).forEach((key) => {
+            formData.append(key, user[key])
+        })
+
+        const data = await api.patch(`/users/edit/${user._id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                "Content-Type": 'multipart/form-data'
+            }
+        }).then((response) => {
+            return response.data;
+        }).catch((err => {
+            msgType = 'error';
+            return err.response.data;
+        }))
+        setFlashMessage(data.message, msgType);
     }
 
 
