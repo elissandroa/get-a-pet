5b1a38b1cf67bcaaac654d23baf8fa2be83277f6
elissandroa <elissandroa@hotmail.com>
2024-09-09 12:38:26 -0300

    Finalizando componente de flash message

--- frontend/src/components/layout/Message.js
+++ frontend/src/components/layout/Message.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Message.css'
+import bus from '../../utils/bus';
 
 export const Message = () => {
+    const [visibility, setVisibility] = useState(false);
+    const [message, setMessage] = useState('')
     const [type, setType] = useState('')
 
+    useEffect(() => {
+        bus.addListener('flash', ({message, type}) => {
+            setVisibility(true)
+            setMessage(message)
+            setType(type)
+            setTimeout(() => {
+                setVisibility(false)
+            }, 3000);
+        })
+    },[])
+
     return (
-        <div className={`message ${type}`}>
-            <h1>Minha mensagem</h1>
+    visibility && <div className={`message ${type}`}>
+            {message}
         </div>
     )
 }
