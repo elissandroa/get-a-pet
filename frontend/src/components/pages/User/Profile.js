c3e184eb40eb8ce1168c32a156ec955575bc0bdf
elissandroa <elissandroa@hotmail.com>
2024-09-09 20:31:32 -0300

    Criando componente de imagens

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -11,8 +11,9 @@ export const Profile = () => {
     const [user, setUser] = useState({});
     const [preview, setPreview] = useState('');
     const [token] = useState(localStorage.getItem('token' || ""))
+    const { setFlashMessage } = useFlashMessage();
 
-    useEffect(()=>{
+    useEffect(() => {
         api.get('/users/checkuser', {
             headers: {
                 Authorization: `Bearer ${JSON.parse(token)}`
@@ -20,7 +21,7 @@ export const Profile = () => {
         }).then((response) => {
             setUser(response.data)
         })
-    },[token])
+    }, [token])
 
     const onFileChange = (e) => {
         setPreview(e.target.files[0]);
@@ -28,11 +29,31 @@ export const Profile = () => {
     }
 
     const handleChange = (e) => {
-
+        setUser({ ...user, [e.target.name]: e.target.value })
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        let msgType = 'success';
+
+        const formData = new FormData();
+
+        await Object.keys(user).forEach((key) => {
+            formData.append(key, user[key])
+        })
+
+        const data = await api.patch(`/users/edit/${user._id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+                "Content-Type": 'multipart/form-data'
+            }
+        }).then((response) => {
+            return response.data;
+        }).catch((err => {
+            msgType = 'error';
+            return err.response.data;
+        }))
+        setFlashMessage(data.message, msgType);
     }
 
 
