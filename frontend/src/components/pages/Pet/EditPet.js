6467a169f6a0589830c4172dbcf40e9aff0c61de
elissandroa <elissandroa@hotmail.com>
2024-09-11 11:19:43 -0300

    Criando página de edição de pet

--- frontend/src/components/pages/Pet/EditPet.js
+++ frontend/src/components/pages/Pet/EditPet.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import api from '../../../utils/api'
+import './AddPet.css'
+import PetForm from '../../form/PetForm'
+
+import fleshMessage from '../../../hooks/useFlashMessage'
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 
 export const EditPet = () => {
+    const { setFlashMessage } = useFlashMessage();
     const { id } = useParams();
     return (
-        <div><h1>Editando o pet ID: {id}</h1></div>
+        <div>
+            <div className="addpet-header">
+                <h1>Editando o Pet: 'pet.name'</h1>
+                <p>Depois da edição os dados serão atualizados no sistema</p>
+            </div>
+        </div>
     )
 }
