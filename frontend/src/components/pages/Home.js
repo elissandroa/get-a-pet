e382762f76ebef44abd889d9b1f5a465b76be1e5
elissandroa <elissandroa@hotmail.com>
2024-09-12 09:18:26 -0300

    CSS da Home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import api from '../../utils/api';
+import './Home.css'
 
 export const Home = () => {
   const [pets, setPets] = useState([]);
@@ -16,25 +17,24 @@ export const Home = () => {
       }).catch((err) => console.log(err));
   }
 
-  console.log(pets)
   return (
     <section>
-      <div>
+      <div className='pet-home-header'>
         <h1>Adote um Pet</h1>
         <p>Veja os detalhes de cada um e conheça os Tutores</p>
       </div>
-      <div>
+      <div className='pet-container'>
         {pets.length > 0 &&
           pets.map((pet) => (
-            <div>
-              <p>Imagem do Pet</p>
+            <div className='pet-card'>
+              <div style={{backgroundImage: `url(${process.env.REACT_APP_API}/images/pets/${pet.images[0]})`}} className="pet-card-image"></div>
               <h3>{pet.name}</h3>
               <p>
                 <span className="bold">
                   Peso: {pet.wheight}Kg
                 </span>
               </p>
-              {pet.available ? <Link to={`pets/${pet._id}`}>Mais Detalhes</Link> : <p>Adotado</p>}
+              {pet.available ? <Link to={`pets/${pet._id}`}>Mais Detalhes</Link> : <p className='adopted-text'>Adotado</p>}
             </div>
           ))}
         {pets.length === 0 && <p>Não há pets cadastrados ou disponíveis para doação no momento!</p>}
