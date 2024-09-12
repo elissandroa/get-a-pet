210b14c24d3ca2545f808d2cdac03e442e0baa7f
elissandroa <elissandroa@hotmail.com>
2024-09-12 12:14:17 -0300

    Buscando pet via api

--- frontend/src/components/pages/Pet/PetDetails.js
+++ frontend/src/components/pages/Pet/PetDetails.js
@@ -11,13 +11,29 @@ export const PetDetails = () => {
     const [pet, setPet] = useState({});
     const { setFlasMessage } = useFlashMessage();
     const { id } = useParams()
+    const [token] = useState(localStorage.getItem('token' || ''));
+
+    useEffect(() => {
+        getAPet(id);
+    }, [])
 
 
     async function getAPet(id) {
-        
+        let msgType = 'success';
+       await api.get(`/pets/${id}`)
+            .then((response) => {
+                setPet(response.data.pet);
+
+            }).catch((err) => {
+                msgType = 'error';
+                return err.response.data;
+            })
     }
-    
+
     return (
-        <div><h1>Pet Details</h1></div>
+        <div>
+            <h1>Pet Details</h1>
+            {pet.name}
+        </div>
     )
 }
