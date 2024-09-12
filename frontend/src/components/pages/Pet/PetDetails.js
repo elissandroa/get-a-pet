070496cf4709e4d40fb773d6652b8387328100c5
elissandroa <elissandroa@hotmail.com>
2024-09-12 14:17:39 -0300

    Estruturando página de pet

--- frontend/src/components/pages/Pet/PetDetails.js
+++ frontend/src/components/pages/Pet/PetDetails.js
@@ -20,7 +20,7 @@ export const PetDetails = () => {
 
     async function getAPet(id) {
         let msgType = 'success';
-       await api.get(`/pets/${id}`)
+        await api.get(`/pets/${id}`)
             .then((response) => {
                 setPet(response.data.pet);
 
@@ -31,9 +31,31 @@ export const PetDetails = () => {
     }
 
     return (
-        <div>
-            <h1>Pet Details</h1>
-            {pet.name}
-        </div>
+        <>
+            {pet.name && (
+                <section>
+                    <div>
+                        <h1>Conhecendo o Pet: {pet.name}</h1>
+                        <p>Se tiver interesse, marque uma visita para conhecê-lo</p>
+                    </div>
+                    <div>
+                        {pet.images.map((image, index) => (
+                            <img
+                                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                                alt={pet.name}
+                                key={index}
+                            />
+                        ))}
+                    </div>
+                    <p>
+                        <span className="bold">Peso: {pet.wheight}Kg</span>
+                        <p>
+                        <span className="bold">Idade: {pet.age} anos</span>
+                    </p>
+                    </p>
+                    {token ? <button>Solicitar uma visita</button> : <p>Você precisa <Link to={'/register'}>criar uma conta</Link> para solicitar a visita!</p>}
+                </section>
+            )}
+        </>
     )
 }
